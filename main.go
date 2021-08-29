253626f100aad7b7979f58b770125032f428a78d
Alex <alex@alexlorei.xyz>
2021-08-29 18:54:41 -0400

    basic card draw and bust implemented

--- main.go
+++ main.go
@@ -1,21 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 var playerAmmount int = 0
 
-var cards = map[string]int{
-	"two":   2,
-	"three": 3,
-	"four":  4,
-	"five":  5,
-	"six":   6,
-	"seven": 7,
-	"eight": 8,
-	"nine":  9,
-	"ten":   10,
-}
+var cards = [9]int{2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 func main() {
-	fmt.Println(cards["three"])
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		if playerAmmount < 21 {
+			fmt.Print("-> ")
+			text, _ := reader.ReadString('\n')
+
+			text = strings.Replace(text, "\n", "", -1)
+			fmt.Println("Would you like to hit")
+			if strings.Compare("yes", text) == 0 {
+				randCard(0)
+				fmt.Println(playerAmmount)
+			}
+		} else if playerAmmount > 21 {
+			fmt.Println("Bust!")
+			os.Exit(0)
+		}
+	}
+}
+
+func randCard(n int) int {
+	rand.Seed(time.Now().UnixNano())
+	in := cards
+	randomIndex := rand.Intn(len(in))
+	pick := in[randomIndex]
+	playerAmmount = playerAmmount + pick
+	return 0
 }
