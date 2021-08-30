48faf6bb4d90b1c6ee310a408f1ffd681d945d9f
x86Cow <62828198+MrCow3@users.noreply.github.com>
2021-08-30 14:07:40 -0400

    Update dealer.go

--- dealer.go
+++ dealer.go
@@ -6,4 +6,27 @@ var dealerAmount int = 0
 
 func main() {
 	fmt.Println(dealerAmount)
+	for {
+		if dealerAmount < 21 {
+			randCard(0)
+		}
+		
+		if dealerAmount > 21 {
+			fmt.Println("Dealer busted!")
+		}
+		
+		if dealerAmount = 21 {
+			fmt.Println("Dealer Blackjack!")
+		}
+		
+	}
+}
+
+func randCard(n int) int {
+	rand.Seed(time.Now().UnixNano())
+	in := cards
+	randomIndex := rand.Intn(len(in))
+	pick := in[randomIndex]
+	dealerAmount = dealerAmount + pick
+	return 0
 }
