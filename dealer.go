8f13cd9ae33d188fac7a79f9733c12424450ece1
Alex <alex@alexlorei.xyz>
2021-09-01 16:35:53 -0400

    updated dealer

--- dealer.go
+++ dealer.go
@@ -2,29 +2,27 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"time"
 )
 
 func dealer() {
 	for {
 		if dealerAmount < 21 {
-			if dealerAmount < 21 {
-				fmt.Println("The dealer hits")
-				fmt.Println(dealerAmount)
-				randCardDealer(0)
-			}
+			fmt.Println("The dealer hits")
+			fmt.Println(dealerAmount)
+			randCardDealer(0)
+			time.Sleep(1 * time.Second)
 		}
 
 		if dealerAmount > 21 {
 			fmt.Println("Dealer busted!")
 			fmt.Println(dealerAmount)
-			os.Exit(0)
+			break
 		}
 
 		if dealerAmount == 21 {
 			fmt.Println("Dealer Blackjack!")
-			os.Exit(0)
+			break
 		}
-
 	}
 }
